16d29dae21e08f50c56a026d169c634df3f52e25
anurag bagri <anurag123bagri@gmail.com>
2025-09-20 12:39:41 +0530

    added profile card

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,20 +1,23 @@
-"use client"
-import ProfileCard from './ProfileCard';
-
+import TiltedCard from './TiltedCard';
 export default function Profile(){
-    return (
-          
-<ProfileCard
-  name="Anurag Bagri"
-  title="Full stack web2 abd web3 Developer"
-  handle="noob_anurag"
-  status="Online"
-  contactText="Contact Me"
-  avatarUrl="./profile-photo.jpg"
-  showUserInfo={true}
-  enableTilt={true}
-  enableMobileTilt={false}
-  onContactClick={() => console.log('Contact clicked')}
+return (
+<TiltedCard
+  imageSrc="https://media.licdn.com/dms/image/v2/D5603AQFirspE0c7wlg/profile-displayphoto-shrink_200_200/B56ZQUEqioG8AY-/0/1735503555490?e=2147483647&v=beta&t=QHcqkdZRg_OMq0vYBaNFQ9bugr9W9gSHm0VXGyTS3dM"
+  altText="Anurag - wassaup "
+  captionText="Anurag"
+  containerHeight="300px"
+  containerWidth="300px"
+  imageHeight="300px"
+  imageWidth="300px"
+  rotateAmplitude={12}
+  scaleOnHover={1.2}
+  showMobileWarning={false}
+  showTooltip={true}
+  displayOverlayContent={true}
+  overlayContent={
+<p className="tilted-card-demo-text text-xl text-shadow-white">
+    </p>
+  }
 />
-    )
+)
 }
